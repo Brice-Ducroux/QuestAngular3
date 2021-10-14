2178c58147c23f78d40b24229eafb97a0132692e
Brice-Ducroux <brice-ducroux_student2021@wilder.school>
2021-10-14 17:48:33 +0200

    first component

---
+++ src/app/user-profile/user-profile.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-profile',
+  templateUrl: './user-profile.component.html',
+  styleUrls: ['./user-profile.component.css'],
+})
+export class UserProfileComponent implements OnInit {
+  User = {
+    name: 'Outang',
+    firstName: 'Laurent',
+    age: 25,
+    quote: 'Pas de cerveau, pas de migraines',
+    photo: 'https://randomuser.me/api/portraits/lego/2.jpg',
+  };
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  showAge: boolean = false;
+  displayAge() {
+    this.showAge = true;
+  }
+}
